39c76020f1b48326aaadc50164009ba9d7b094f5
hayha06 <brice06250@hotmail.fr>
2020-02-13 18:36:34 +0100

    add input to check or not check regex

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const github = require('@actions/github');
 
 async function run() {
     try {
+
+        isTitleShouldContainPackageVersionIncrementation = core.getInput('isTitleShouldContainPackageVersionIncrementation', { required: true });
         const title = github.context.payload.pull_request.title;
         core.debug(`title: ${title}`);
 
         const titleRegex = /(\+semver:\s?(breaking|major)|\+semver:\s?(feature|minor)|\+semver:\s?(fix|patch)|\+semver:\s?(none|skip))/;
         const matches = titleRegex.test(title);
-        if (!matches) {
-            core.setFailed('Pull request title does not match given regex, it should include either one of this option : [+semver: breaking, +semver: feature, +semver: fix, +semver: none] ');
+        if (isTitleShouldContainPackageVersionIncrementation && !matches) {
+            core.setFailed('Pull request title does not match given regex, it should include either one of this option : [+semver: breaking|major, +semver: feature|minor, +semver: fix|patch, +semver: none|skip] ');
+            return;
+        }
+        if (!isTitleShouldContainPackageVersionIncrementation && matches) {
+            core.setFailed('Pull request title should not include one of this : [+semver: breaking|major, +semver: feature|minor, +semver: fix|patch, +semver: none|skip] if the library was not updated');
             return;
         }
     }
